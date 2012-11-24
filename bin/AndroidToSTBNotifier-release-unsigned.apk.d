3989a5c627771181b966112ed802ace974610236
hyperonex <achraf_g@yahoo.fr>
2012-11-24 14:37:01 +0300

    Code cleaning

--- bin/AndroidToSTBNotifier-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/achraf/Developpement/PhoneToSTBNotifier/bin/AndroidToSTBNotifier-release-unsigned.apk : \
-/home/achraf/Developpement/PhoneToSTBNotifier/bin/AndroidToSTBNotifier.ap_ \
-/home/achraf/Developpement/PhoneToSTBNotifier/bin/classes.dex \
-/home/achraf/Developpement/android-sdk-linux/tools/support/annotations.jar \
